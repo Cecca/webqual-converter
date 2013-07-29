42ce9af95103314fa5133b8333780121cda8e4b1
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-07-29 09:35:09 +0200

    Add comand line option to process links file

--- src/Main.hs
+++ src/Main.hs
@@ -8,11 +8,17 @@ main::IO()
 main = do
     args <- getArgs
     case args of
-      [input,output] -> do
-        inData <- BSC.readFile input
-        let pairs = (processLines . BSC.lines) inData
-        let outData = BSC.unlines $ map pairToStr pairs
-        BSC.writeFile output outData
-        print "Done"
+      ["-u",input,output] -> do
+          inData <- BSC.readFile input
+          let pairs = (processLines . BSC.lines) inData
+          let outData = BSC.unlines $ map pairToStr pairs
+          BSC.writeFile output outData
+          print "Done"
+      ["-l",input,output] -> do
+          inData <- BSC.readFile input
+          let origHashes = groupHashes inData
+          let smallHashes = map get64bitHash origHashes
+          BSC.writeFile output $ unGroupHashes smallHashes
+          print "Done"
       _ -> error "Please provide both input and output filenames"
 
