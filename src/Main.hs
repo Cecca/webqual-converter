c8cd118c0a4aadd7020a42678213aae0ce920700
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-07-29 17:27:09 +0200

    Move conversion from main function

--- src/Main.hs
+++ src/Main.hs
@@ -2,23 +2,33 @@ module Main where
 
 import System.Environment (getArgs)
 import Converter
-import Data.ByteString.Lazy.Char8 as BSC hiding (map)
+import Data.ByteString.Lazy.Char8 as BSC hiding (map, putStrLn)
 
-main::IO()
+main :: IO()
 main = do
     args <- getArgs
     case args of
       ["-u",input,output] -> do
-          inData <- BSC.readFile input
-          let pairs = (processLines . BSC.lines) inData
-          let outData = BSC.unlines $ map pairToStr pairs
-          BSC.writeFile output outData
-          print "Done"
+          convertUrlsFile input output
+          putStrLn $ "Converted " ++ input
       ["-l",input,output] -> do
-          inData <- BSC.readFile input
-          let origHashes = groupHashes inData
-          let smallHashes = map get64bitHash origHashes
-          BSC.writeFile output $ unGroupHashes smallHashes
-          print "Done"
+          convertLinksFile input output
+          putStrLn $ "Converted " ++ input
       _ -> error "Please provide both input and output filenames"
+
+
+convertUrlsFile :: FilePath -> FilePath -> IO ()
+convertUrlsFile input output = do
+    inData <- BSC.readFile input
+    let pairs = (processLines . BSC.lines) inData
+    let outData = BSC.unlines $ map pairToStr pairs
+    BSC.writeFile output outData
+
+convertLinksFile :: FilePath -> FilePath -> IO ()
+convertLinksFile input output = do
+    inData <- BSC.readFile input
+    let origHashes = groupHashes inData
+    let smallHashes = map get64bitHash origHashes
+    BSC.writeFile output $ unGroupHashes smallHashes
+
 
