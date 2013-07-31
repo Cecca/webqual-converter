63d56f3039437ed926dc88f4f3f3715353b15a3d
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2013-07-31 23:25:55 +0200

    Add benchmark for processLinks
    It shows that parallel code with two threads is twice as fast as serial
    code

--- src/Bench.hs
+++ src/Bench.hs
@@ -2,6 +2,8 @@ import Criterion.Main
 import Criterion.Config
 import Converter
 import qualified Data.ByteString.Lazy.Char8 as BSC
+import qualified Data.ByteString.Lazy as BS
+import Crypto.Hash
 
 main :: IO ()
 main = defaultMainWith defaultConfig (return ()) [
@@ -12,9 +14,26 @@ main = defaultMainWith defaultConfig (return ()) [
             bench "processUrls 4000" $ whnf processUrls (lns 4000),
             bench "processUrls 5000" $ whnf processUrls (lns 5000),
             bench "processUrls 6000" $ whnf processUrls (lns 6000)
+          ],
+          bgroup "processLinks" [
+            bench "processLinks 10k" $ whnf processLinks (links 10000),
+            bench "processLinks 20k" $ whnf processLinks (links 20000),
+            bench "processLinks 30k" $ whnf processLinks (links 30000),
+            bench "processLinks 40k" $ whnf processLinks (links 40000),
+            bench "processLinks 50k" $ whnf processLinks (links 50000),
+            bench "processLinks 60k" $ whnf processLinks (links 60000),
+            bench "processLinks 70k" $ whnf processLinks (links 70000)
           ]
         ]
 
 lns :: Int -> BSC.ByteString
 lns n = BSC.concat $ take n $ cycle [BSC.pack "line"]
+
+links :: Int -> BS.ByteString
+links n = BS.concat $ take n $ cycle [h]
+    where h = BS.fromChunks [h']
+          h' = digestToByteString $ md5 $ BSC.pack "stringToDigest"
+
+md5 :: BS.ByteString -> Digest MD5
+md5 = hashlazy
 
